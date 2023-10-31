e2929975af1e9f5075f15d643050e91385eebad4
Ayush Sharma <ayushxsharma0@gmail.com>
2023-10-31 20:27:01 +0530

    Update missing_number.cpp

--- Introductory/missing_number.cpp
+++ Introductory/missing_number.cpp
@@ -1,26 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
-int function1(int arr[], int n){
-    //this typically sort the array and then iterate through it to find the missing element
-    sort(arr,arr+n-1) ;
-    int k =1 ;
-    for(int i=0;i<n-1;i++){
-        if(arr[i] != k){
-            return k ;
-        }
-        k++ ;
+// int function1(int arr[], int n){
+//     //this typically sort the array and then iterate through it to find the missing element
+//     sort(arr,arr+n-1) ;
+//     int k =1 ;
+//     for(int i=0;i<n-1;i++){
+//         if(arr[i] != k){
+//             return k ;
+//         }
+//         k++ ;
+//     }
+//     //time complexity = O(nlogn) cuz of sorting
+// }
+
+int function2(long long arr[],long long n){
+    //we ca calculate sum upto n and minus our sum to get the number
+    long long sum =0 ;
+    for(int i =0;i<n-1;i++){
+        sum += arr[i] ;
     }
-    //time complexity = O(nlogn) cuz of sorting
+    //sum of first n integers = n(n-1)/2 ;
+    long long realsum = n*(n+1)/2 ;
+    return  realsum - sum ;
+    //time complexity = O(n)
 }
 
 int main(){
-    int n ; //size of array
+    long long n ; //size of array
     cin>>n ;
 
-    int arr[n] ;
+    long long arr[n] ;
     for(int i =0;i<n-1;i++){
         cin>>arr[i] ;
     }
-    cout<<function1(arr,n) ;
+    cout<<function2(arr,n) ;
 }
